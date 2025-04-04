f738076a1113771e658743986cd840c9d9c51723
jailson-p <jailson.p@aluno.ifsc.edu.br>
2025-04-04 11:44:59 -0300

    Exec2: Padronização das variáveis constantes

--- exercicio-2/app/src/main/java/org/example/App.java
+++ exercicio-2/app/src/main/java/org/example/App.java
@@ -2,8 +2,24 @@ package org.example;
 
 public class App {
 
+    private static final String[] PRETO       = {"preto",       "0",    ""          };
+    private static final String[] MARROM      = {"marrom",      "1",    "+- 1%"     };
+    private static final String[] VERMELHO    = {"vermelho",    "2",    "+- 2%"     };
+    private static final String[] LARANJA     = {"laranja",     "3",    ""          };
+    private static final String[] AMARELO     = {"amarelo",     "4",    ""          };
+    private static final String[] VERDE       = {"verde",       "5",    "+- 0,5%"   };
+    private static final String[] AZUL        = {"azul",        "6",    "+- 0,25%"  };
+    private static final String[] VIOLETA     = {"violeta",     "7",    "+- 0,1%"   };
+    private static final String[] CINZA       = {"cinza",       "8",    ""          };
+    private static final String[] BRANCO      = {"branco",      "9",    ""          };
+    private static final String[] OURO        = {"ouro",        "0.1",  "+- 5%"     };
+    private static final String[] PRATA       = {"prata",       "0.01", "+- 10%"    };
+    private static final String[] NULO        ={"",             "",     "+- 20%"    };
+
     public static void main(String[] args) {
 
 
-       }
+
+    }
+
 }
